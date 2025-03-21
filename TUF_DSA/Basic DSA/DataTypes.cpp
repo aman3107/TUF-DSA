df10907980ea073cd73d5e2fe2260affb303e3f5
Aman Mittal <amanmittalma@gmail.com>
2025-03-21 23:42:56 +0530

    Basic CPP

---
+++ TUF_DSA/Basic DSA/DataTypes.cpp
@@ -0,0 +1,35 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+int main()
+{
+  int numInt = INT_MAX;
+  cout << numInt << endl;
+
+  long num_long = 1000000000;
+  cout << num_long << endl;
+
+  long long numLong = 1000000000000000000;
+  cout << numLong << endl;
+
+  float fl = 8.1234;
+  cout << fl << endl;
+
+  double db = 9.1234567;
+  cout << db << endl;
+
+  char ch = 'a';
+  cout << ch << endl;
+
+  string str = "aman";
+  cout << str << endl;
+
+  bool bl = true;
+  cout << bl << endl;
+
+  getline(cin, str);
+  cout << str << endl;
+}
