7c12388bdb26665ce979fae41d10fe44fb96d290
Aman Mittal <amanmittalma@gmail.com>
2025-04-15 22:52:40 +0530

    Single element in sorted array

--- TUF_DSA/Advanced_DSA/Binary_Search/Logic_Building/Single_Element_In_Sorted_Array.cpp
+++ TUF_DSA/Advanced_DSA/Binary_Search/Logic_Building/Single_Element_In_Sorted_Array.cpp
@@ -1,17 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int singleNonDuplicateBrute(vector<int> &nums)
+int singleNonDuplicate(vector<int> &nums)
 {
   int n = nums.size();
-  for (int i = 1; i < n; i = i + 2)
+  if (n == 1)
   {
-    if (nums[i - 1] != nums[i])
+    return nums[0];
+  }
+  if (nums[0] != nums[1])
+    return nums[0];
+  if (nums[n - 1] != nums[n - 2])
+  {
+    return nums[n - 1];
+  }
+  int low = 1;
+  int high = n - 2;
+  while (low <= high)
+  {
+    int mid = low + (high - low) / 2;
+    if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1])
     {
-      return nums[i];
+      return nums[mid];
+    }
+    if ((mid % 2 == 1 && nums[mid - 1] == nums[mid]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+    {
+      low = mid + 1;
+    }
+    else
+    {
+      high = mid - 1;
     }
   }
-  return 0;
+}
+
+int singleNonDuplicateBruteXOR(vector<int> &nums)
+{
+  int n = nums.size();
+  int ans = 0;
+  for (int i = 0; i < n; i++)
+  {
+    ans = ans ^ nums[i];
+  }
+  return ans;
+}
+
+int singleNonDuplicateBrute(vector<int> &nums)
+{
+
+  int n = nums.size();
+  if (n == 1)
+  {
+    return nums[0];
+  }
+  for (int i = 0; i < n; i++)
+  {
+    if (i == 0)
+    {
+      if (nums[i] != nums[i + 1])
+      {
+        return nums[i];
+      }
+    }
+    else if (i == n - 1)
+    {
+      if (nums[i - 1] != nums[i])
+      {
+        return nums[i];
+      }
+    }
+    else
+    {
+      if (nums[i - 1] != nums[i] && nums[i] != nums[i + 1])
+      {
+        return nums[i];
+      }
+    }
+  }
+  return -1;
 }
 
 int main()
@@ -25,5 +91,5 @@ int main()
     cin >> val;
     v.push_back(val);
   }
-  cout << singleNonDuplicateBrute(v) << endl;
+  cout << singleNonDuplicateBruteXOR(v) << endl;
 }
