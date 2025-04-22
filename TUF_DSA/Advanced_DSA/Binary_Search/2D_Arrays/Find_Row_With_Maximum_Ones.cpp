63a600be35fba3f3e13b5b730ed083f5038faaf5
Aman Mittal <amanmittalma@gmail.com>
2025-04-22 23:35:30 +0530

    Row With Maximum Ones

--- TUF_DSA/Advanced_DSA/Binary_Search/2D_Arrays/Find_Row_With_Maximum_Ones.cpp
+++ TUF_DSA/Advanced_DSA/Binary_Search/2D_Arrays/Find_Row_With_Maximum_Ones.cpp
@@ -1,8 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int lowerBound(vector<int> &arr, int n, int x)
+{
+  int low = 0;
+  int high = n - 1;
+  int ans = n;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    if (arr[mid] >= x)
+    {
+      ans = mid;
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
 int rowWithMax1s(vector<vector<int>> &mat)
 {
+  int maxCount = 0;
+  int index = -1;
+  int n = mat.size();
+  int m = mat[0].size();
+  for (int i = 0; i < n; i++)
+  {
+    int countOnes = m - lowerBound(mat[i], m, 1);
+    if (countOnes > maxCount)
+    {
+      maxCount = countOnes;
+      index = i;
+    }
+  }
+  return index;
+}
+
+int rowWithMax1sBrute(vector<vector<int>> &mat)
+{
+  int maxCount = 0;
+  int index = -1;
+  int n = mat.size();
+  int m = mat[0].size();
+  for (int i = 0; i < n; i++)
+  {
+    int countOnes = 0;
+    for (int j = 0; j < m; j++)
+    {
+      if (mat[i][j] == 1)
+      {
+        countOnes++;
+      }
+    }
+    if (countOnes > maxCount)
+    {
+      maxCount = countOnes;
+      index = i;
+    }
+  }
+  return index;
 }
 
 int main()
@@ -21,4 +81,5 @@ int main()
     }
     mat.push_back(v);
   }
+  cout << rowWithMax1s(mat) << endl;
 }
